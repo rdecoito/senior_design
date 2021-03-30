15c365df9661b50a111113061ca13376219009cc
Landon <rdecoito@gmu.edu>
2021-03-30 17:29:45 -0400

    continued

--- testing/https_test/config.h
+++ testing/https_test/config.h
@@ -1,5 +1,5 @@
 // The target websites SHA1 fingerprint, used for TLS connections
-const char TRGT_SHA_FINGERPR[] = "40:42:92:A3:1F:AB:7F:CB:BE:A4:CC:81:74:06:78:7E:B7:3F:85:5D";
+//const char TRGT_SHA_FINGERPR[] = "40:42:92:A3:1F:AB:7F:CB:BE:A4:CC:81:74:06:78:7E:B7:3F:85:5D";
 
 // The SSID and password of the desired WiFi network
 const char TRGT_SSID[] = "ssid";
--- testing/https_test/https_test.ino
+++ testing/https_test/https_test.ino
@@ -1,9 +1,13 @@
-#include <ESP8266HTTPClient.h>
+//#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
 #include "config.h"
+#include "certificates.h"
 
 WiFiClientSecure client;
+X509List certList(certificates);
 char clientByte;
+const char* host = "gutenberg.org";
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,27 +18,66 @@ void setup() {
   Serial.println("");
 
   WiFi.begin(TRGT_SSID, TRGT_PASS);
-  while(WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.print("\nConnected!\n    IP: ");
   Serial.println(WiFi.localIP());
 
-  client.setFingerprint(TRGT_SHA_FINGERPR);
-  Serial.println("Attempting to connect");
-  client.connect("gutenberg.org", 443);
-  while (!client.connected()) {
+  // TIME SYNCHRONIZATION
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
     delay(500);
     Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
+
+  // 
+  client.setTrustAnchors(&certList);
+  Serial.println("Attempting to connect");
+  if !(client.connect(host, 443)) {
+    Serial.println("Connection failed");
+    return;
   }
   Serial.println("connected to gutenberg");
-  client.println("GET  /files/60321/60321-h/60321-h.htm HTTP/1.0\n");
+  client.println("GET  /files/60321/60321-h/60321-h.htm HTTP/1.1\r\n"
+    + "Host: " + host + "\r\n" +
+    "User-Agent: BuildFailureDetectorESP8266\r\n" +
+    "Connection: close\r\n\r\n");
+  Serial.println("Request sent");
+
+
+  Serial.println("Request sent");
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
+      Serial.println("Headers received");
+      break;
+    }
+  }
+  String line = client.readStringUntil('\n');
+  if (line.startsWith("{\"state\":\"success\"")) {
+    Serial.println("esp8266/Arduino CI successfull!");
+  } else {
+    Serial.println("esp8266/Arduino CI has failed");
+  }
+  Serial.println("Reply was:");
+  Serial.println("==========");
+  Serial.println(line);
+  Serial.println("==========");
+  Serial.println("Closing connection");
 }
 
 void loop() {
-  if (client.available()) {
-    clientByte = client.read();
-    Serial.print(clientByte);
-  }
+//  if (client.available()) {
+//    clientByte = client.read();
+//    Serial.print(clientByte);
+//  }
 }
